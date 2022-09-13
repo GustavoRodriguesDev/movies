5cfed1e394aa7864c0e163802e59279fdea37712
Gustavo Rodrigues Pereira dos Santos <94488085+GustavoRodriguesDev@users.noreply.github.com>
2022-09-12 22:29:12 -0300

    implementação do ci

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -12,6 +12,7 @@ class _AppWidgetState extends State<AppWidget> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Movies',
       home: HomePage(),
     );
