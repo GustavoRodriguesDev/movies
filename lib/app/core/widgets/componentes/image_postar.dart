ff9eb7d599aa51369d3f4c5f3a1154242effaa44
GustavoRodriguesDev <gustavorpsantos1999@gmail.com>
2022-09-18 23:23:50 -0300

    widget de postar

---
+++ lib/app/core/widgets/componentes/image_postar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../card/movie_card_shimmer.dart';
+
+class ImagePoster extends StatelessWidget {
+  final String pathImage;
+  const ImagePoster({Key? key, required this.pathImage}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      height: height * 0.30,
+      width: width * 0.45,
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(width * 0.05)),
+        color: Colors.white.withOpacity(0.1),
+      ),
+      child: Image.network(
+        'https://image.tmdb.org/t/p/w500$pathImage',
+        fit: BoxFit.fill,
+        errorBuilder: (context, error, stackTrace) {
+          return const Center(child: Text('Image not found'));
+        },
+        loadingBuilder: (_, widget, image) {
+          if (image == null) {
+            return widget;
+          }
+          return const MovieCardShimmer();
+        },
+      ),
+    );
+  }
+}
