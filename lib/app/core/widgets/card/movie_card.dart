2370f4423f50f2d36a7840a2a963f2b0a38083d5
GustavoRodriguesDev <gustavorpsantos1999@gmail.com>
2022-09-20 20:02:27 -0300

    refactor no widget para remover a navegação de dentro dele

--- lib/app/core/widgets/card/movie_card.dart
+++ lib/app/core/widgets/card/movie_card.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
-import '../../../modules/home/subpage/details_movie.dart';
 import '../componentes/image_postar.dart';
 import '../componentes/star_rating.dart';
-import '../navigator/custom_navigator.dart';
 
 class MovieCard extends StatelessWidget {
   final String pathImage;
   final String nameMovie;
   final double ratingMovie;
-  final Image imageBackgroud;
-  final int votes;
-  final String description;
+
+  final void Function()? onTap;
   const MovieCard({
     Key? key,
     required this.pathImage,
     required this.nameMovie,
     required this.ratingMovie,
-    required this.imageBackgroud,
-    required this.votes,
-    required this.description,
+    this.onTap,
   }) : super(key: key);
 
   @override
@@ -26,19 +21,7 @@ class MovieCard extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return GestureDetector(
-      onTap: () {
-        CustomNavigator.pushSlidesTransition(
-          context,
-          DetailsMovie(
-            imageBackgroud: imageBackgroud,
-            pathImage: pathImage,
-            nameMovie: nameMovie,
-            rating: ratingMovie,
-            votes: votes,
-            description: description,
-          ),
-        );
-      },
+      onTap: onTap,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
