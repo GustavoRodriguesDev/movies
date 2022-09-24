ef1601c64b3937e6a85548ba143e719f492cd085
GustavoRodriguesDev <gustavorpsantos1999@gmail.com>
2022-09-24 17:43:29 -0300

    injeção  da store de pesquisar os filmes

--- lib/app/get_it.dart
+++ lib/app/get_it.dart
@@ -3,8 +3,10 @@ import 'package:get_it/get_it.dart';
 import 'core/constants/movies_api.dart';
 import 'core/service/http_service/http_service.dart';
 import 'modules/home/store/home_store.dart';
+import 'modules/home/subpage/search_movie/store/search_movie_store.dart';
 import 'modules/movies/domain/repository/movies_repository.dart';
 import 'modules/movies/domain/usecase/fetch_all_movies_usecase.dart';
+import 'modules/movies/domain/usecase/search_movie.dart';
 import 'modules/movies/external/datasource/movies_datasource.dart';
 import 'modules/movies/infra/datasource/movies_datasource.dart';
 import 'modules/movies/infra/repository/moview_repository.dart';
@@ -31,5 +33,7 @@ void setup() {
   getIt.registerSingleton<IMoviesDatasource>(MoviesDatasource(getIt.get<IHttpService>()));
   getIt.registerSingleton<IMoviesRepository>(MoviesRepository(getIt.get<IMoviesDatasource>()));
   getIt.registerSingleton<IFetchAllMoviesUsecase>(FetchAllMoviesUsecase(getIt.get<IMoviesRepository>()));
+  getIt.registerSingleton<ISearchMovieUsecase>(SearchMovieUsecase(getIt.get<IMoviesRepository>()));
   getIt.registerSingleton<HomeStore>(HomeStore(getIt.get<IFetchAllMoviesUsecase>()));
+  getIt.registerSingleton<SearchMovieStore>(SearchMovieStore(getIt.get<ISearchMovieUsecase>()));
 }
