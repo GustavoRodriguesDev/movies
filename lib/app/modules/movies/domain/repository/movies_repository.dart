fd7f9ae88dcca63d019b8372e0d098a327470dbd
GustavoRodriguesDev <gustavorpsantos1999@gmail.com>
2022-09-24 17:46:29 -0300

    corerção de indentação

--- lib/app/modules/movies/domain/repository/movies_repository.dart
+++ lib/app/modules/movies/domain/repository/movies_repository.dart
@@ -1,10 +1,6 @@
-
 import '../dto/pagination_movie_dto.dart';
 import '../dto/paran_search_movie_dto.dart';
 import '../types/type_movies.dart';
-
-
-
 
 abstract class IMoviesRepository {
   ReturnFetchMovies getAllMovies(PaginationMovieDto page);
