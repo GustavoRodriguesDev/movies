f8430cd1a6c4879323bac18d06f492d228b690c1
GustavoRodriguesDev <gustavorpsantos1999@gmail.com>
2022-09-24 17:44:47 -0300

    Correção por alguns campos da api estão vindo nulos

--- lib/app/modules/movies/external/mapper/movies_mapper.dart
+++ lib/app/modules/movies/external/mapper/movies_mapper.dart
@@ -4,25 +4,25 @@ import '../error/movie_mapper_error.dart';
 class MoviesMapper {
   static List<MovieEntity> fromJson(List<dynamic> movies) {
     try {
-      return movies
-          .map(
-            (e) => MovieEntity(
-              adult: e['adult'],
-              backdropPath: e['backdrop_path'],
-              genreIds: e['genre_ids'],
-              movieID: e['id'],
-              originalLanguage: e['original_language'],
-              originalTitle: e['original_title'],
-              overview: e['overview'],
-              popularity: e['popularity'],
-              posterPath: e['poster_path'],
-              releaseDate: e['release_date'],
-              title: e['title'],
-              voteAverage: e['vote_average'],
-              voteCount: e['vote_count'],
-            ),
-          )
-          .toList();
+      return movies.map(
+        (e) {
+          return MovieEntity(
+            adult: e['adult'],
+            backdropPath: e['backdrop_path'] ?? '',
+            genreIds: e['genre_ids'],
+            movieID: e['id'],
+            originalLanguage: e['original_language'],
+            originalTitle: e['original_title'],
+            overview: e['overview'],
+            popularity: e['popularity'],
+            posterPath: e['poster_path'] ?? '',
+            releaseDate: e['release_date'],
+            title: e['title'],
+            voteAverage: e['vote_average'],
+            voteCount: e['vote_count'],
+          );
+        },
+      ).toList();
     } catch (e, s) {
       throw MovieMapperError(message: e.toString(), stackTrace: s);
     }
