68d217c1afaa91d1b6062637c1fe03f6071ea46a
GustavoRodriguesDev <gustavorpsantos1999@gmail.com>
2022-11-12 22:51:54 -0300

    Adicionado novos edpoints

--- lib/app/core/constants/movies_api.dart
+++ lib/app/core/constants/movies_api.dart
@@ -2,5 +2,9 @@ class ApiConstants {
   static const baseUrl = 'https://api.themoviedb.org/3';
   static const nowPlaying = '/movie/now_playing';
   static const searchMovie = '/search/movie';
+  static const person = '/person';
+  static const movieCredtis = '/movie_credtis';
   static const image = 'https://image.tmdb.org/t/p/w500';
+  static const movie = '/movie';
+  static const credits = '/credits';
 }
