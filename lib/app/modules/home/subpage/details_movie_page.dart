ac36bd584d849c5d664ab25b95b9116408bf2efb
GustavoRodriguesDev <gustavorpsantos1999@gmail.com>
2022-11-13 23:15:30 -0300

    Adcionado scroll para pagina e adciionado card de cast

--- lib/app/modules/home/subpage/details_movie_page.dart
+++ lib/app/modules/home/subpage/details_movie_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:movies/app/modules/home/subpage/store/details_movies_store.dart';
 
 import '../../../core/constants/movies_api.dart';
+import '../../../core/widgets/shimmer/cast_card_shimmer.dart';
+import 'store/state/state_details_movies.dart';
 
 class DetailsMovie extends StatefulWidget {
   final String imageBackgroud;
@@ -40,121 +42,208 @@ class _DetailsMovieState extends State<DetailsMovie> {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              SizedBox(
-                height: height * 0.35,
-                width: width,
-                child: Image.network(
-                  ApiConstants.image + widget.imageBackgroud,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return const Center(
-                        child: Text(
-                      'Image not found',
-                      style: TextStyle(color: Colors.white),
-                    ));
-                  },
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                child: Container(
-                  height: height * 0.15,
-                  width: width,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                      colors: [
-                        const Color(0xFF1B2230),
-                        const Color(0xFF1B2230).withOpacity(0.2),
-                        Colors.transparent,
-                      ],
-                      stops: const [0.0, 0.6, 1],
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: height * 0.05,
-                left: width * 0.83,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Container(
-                    height: width * 0.1,
-                    width: width * 0.1,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.4),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                    child: Icon(
-                      Icons.close_rounded,
-                      size: width * 0.08,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
               children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.star,
-                      color: Colors.yellow,
-                    ),
-                    Text(
-                      '${widget.rating} ',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: width * 0.05,
-                      ),
-                    ),
-                    Text(
-                      '| ${widget.votes}',
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.6),
-                        fontSize: width * 0.04,
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: width * 0.04),
-                Text(
-                  widget.nameMovie,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: width * 0.06,
+                SizedBox(
+                  height: height * 0.35,
+                  width: width,
+                  child: Image.network(
+                    ApiConstants.image + widget.imageBackgroud,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return const Center(
+                          child: Text(
+                        'Image not found',
+                        style: TextStyle(color: Colors.white),
+                      ));
+                    },
                   ),
                 ),
-                SizedBox(height: width * 0.04),
-                Text(
-                  widget.description,
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.6),
-                    fontSize: width * 0.04,
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    height: height * 0.15,
+                    width: width,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                          const Color(0xFF1B2230),
+                          const Color(0xFF1B2230).withOpacity(0.2),
+                          Colors.transparent,
+                        ],
+                        stops: const [0.0, 0.6, 1],
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: height * 0.05,
+                  left: width * 0.83,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Container(
+                      height: width * 0.1,
+                      width: width * 0.1,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.4),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: Icon(
+                        Icons.close_rounded,
+                        size: width * 0.08,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 )
               ],
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.star,
+                        color: Colors.yellow,
+                      ),
+                      Text(
+                        '${widget.rating} ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: width * 0.05,
+                        ),
+                      ),
+                      Text(
+                        '| ${widget.votes}',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.6),
+                          fontSize: width * 0.04,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: width * 0.04),
+                  Text(
+                    widget.nameMovie,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: width * 0.06,
+                    ),
+                  ),
+                  SizedBox(height: width * 0.04),
+                  Text(
+                    widget.description,
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.6),
+                      fontSize: width * 0.04,
+                    ),
+                  ),
+                  SizedBox(height: width * 0.04),
+                  Text(
+                    'Elenco',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: width * 0.06,
+                    ),
+                  ),
+                  SizedBox(height: width * 0.04),
+                  SizedBox(
+                    height: 165,
+                    width: width,
+                    child: ValueListenableBuilder<DetailsMoviesState>(
+                      valueListenable: detaisMoviesStore,
+                      builder: (context, value, child) {
+                        if (value is LoadingDetailsMoviesState) {
+                          SizedBox(
+                            height: 90,
+                            child: ListView.builder(
+                              itemCount: 5,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) {
+                                return const Center(child: CastCardShimmer());
+                              },
+                            ),
+                          );
+                        }
+                        if (value is ErrorDetailsMoviesState) {}
+                        if (value is SuccessDetailsMoviesState) {
+                          return ListView.builder(
+                            itemCount: value.listCast.length,
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              final cast = value.listCast[index];
+                              return Column(
+                                children: [
+                                  Container(
+                                    height: 90,
+                                    width: 90,
+                                    margin: const EdgeInsets.symmetric(horizontal: 6),
+                                    clipBehavior: Clip.antiAlias,
+                                    decoration: const BoxDecoration(shape: BoxShape.circle),
+                                    child: Image.network(
+                                      ApiConstants.image + cast.profilePath,
+                                      fit: BoxFit.cover,
+                                      errorBuilder: (context, error, stackTrace) {
+                                        return const Center(
+                                            child: Text(
+                                          'Image not found',
+                                          style: TextStyle(color: Colors.white),
+                                        ));
+                                      },
+                                      loadingBuilder: (_, widget, image) {
+                                        if (image == null) {
+                                          return widget;
+                                        }
+
+                                        return const CastCardShimmer();
+                                      },
+                                    ),
+                                  ),
+                                  SizedBox(height: width * 0.02),
+                                  SizedBox(
+                                    width: 90,
+                                    child: Text(
+                                      cast.originalName,
+                                      // overflow: TextOverflow.ellipsis,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.white.withOpacity(0.6),
+                                        fontSize: width * 0.04,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              );
+                            },
+                          );
+                        }
+                        return Container();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
